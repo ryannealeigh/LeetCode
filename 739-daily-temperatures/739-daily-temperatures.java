baa7595468f7df5dc5a24a8699f7980e408a4e15
Ryan Nealeigh <ryan.nealeigh@gmail.com>
2022-05-25 06:39:51 -0700

    Time: 52 ms (73.18%), Space: 130.3 MB (41.95%) - LeetHub

---
+++ 739-daily-temperatures/739-daily-temperatures.java
@@ -0,0 +1,21 @@
+class Solution {
+    public int[] dailyTemperatures(int[] temperatures) {
+        // next greatest element for each element
+        ArrayDeque<Integer> stack = new ArrayDeque<>();
+        int[] result = new int[temperatures.length];
+        
+        for (int i = temperatures.length - 1; i >= 0; i--) {
+            while (!stack.isEmpty() && temperatures[i] >= temperatures[stack.peek()]) {
+                stack.pop();
+            }
+            if (stack.isEmpty()) {
+                result[i] = 0;
+            } else {
+                result[i] = stack.peek() - i;
+            }
+            stack.push(i);
+        }
+        
+        return result;
+    }
+}
