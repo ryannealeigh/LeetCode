cd9760153d86588a35470b28466504af9607f4ae
Ryan Nealeigh <ryan.nealeigh@gmail.com>
2022-05-25 13:07:51 -0700

    Time: 125 ms (64.70%), Space: 109.6 MB (8.46%) - LeetHub

--- 853-car-fleet/853-car-fleet.java
+++ 853-car-fleet/853-car-fleet.java
@@ -1,26 +1,24 @@
 class Solution {
-    public int carFleet(int target, int[] position, int[] speed) {
-        List<List<Integer>> pairs = new ArrayList<>();
-        
-        for (int i = 0; i < position.length; i++) {
-            pairs.add(List.of(position[i], speed[i]));
-        }
-        
-        Collections.sort(pairs, (a, b) -> a.get(0) - b.get(0));
-        int n = pairs.size();
-        
-        double slowest = ((double) target - pairs.get(n - 1).get(0)) / pairs.get(n - 1).get(1);
-        int count = 1;
-        
-        for (int i = n - 2; i >= 0; i--) {
-            double timeToTarget = ((double) target - pairs.get(i).get(0)) / pairs.get(i).get(1);
-
-            if (timeToTarget > slowest) {
-                slowest = timeToTarget;
-                count++;
-            }
-        }
-        
-        return count;
-    }
+	public int carFleet(int target, int[] position, int[] speed) {
+		int n = position.length;
+		int[] dist = new int[n];
+		double[] timeAr = new double[n];
+		HashMap<Integer, Double> map = new HashMap<>();
+		for (int i = 0; i < n; i++) {
+			dist[i] = target - position[i]; //calculate distance left for each car
+			timeAr[i] = (double) dist[i] / (double) speed[i]; //calculate original time needed for each car
+			map.put(dist[i], timeAr[i]); //map distance left with original time needed
+		}
+		
+		Arrays.sort(dist); //the cars with larger distance left & smaller time needed will chase up the car before
+		List<Double> rTimeL = new ArrayList<Double>(); //store time used for each fleet in ascending order
+		for (int i = 0; i < n; i++) {
+			Double time = map.get(dist[i]);
+			int m = rTimeL.size();
+			if (m == 0 || time > rTimeL.get(m - 1)) { 
+				rTimeL.add(time); //new fleet is created
+			}
+		}
+		return rTimeL.size();
+	}
 }
