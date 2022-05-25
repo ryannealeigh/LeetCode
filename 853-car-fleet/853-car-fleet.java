557988b2b3775b32edfb27c5c94f713fe1f68f51
Ryan Nealeigh <ryan.nealeigh@gmail.com>
2022-05-25 13:18:22 -0700

    Time: 95 ms (88.21%), Space: 57.2 MB (79.81%) - LeetHub

--- 853-car-fleet/853-car-fleet.java
+++ 853-car-fleet/853-car-fleet.java
@@ -1,24 +1,28 @@
 class Solution {
-	public int carFleet(int target, int[] position, int[] speed) {
-		int n = position.length;
-		int[] dist = new int[n];
-		double[] timeAr = new double[n];
-		HashMap<Integer, Double> map = new HashMap<>();
-		for (int i = 0; i < n; i++) {
-			dist[i] = target - position[i]; //calculate distance left for each car
-			timeAr[i] = (double) dist[i] / (double) speed[i]; //calculate original time needed for each car
-			map.put(dist[i], timeAr[i]); //map distance left with original time needed
-		}
-		
-		Arrays.sort(dist); //the cars with larger distance left & smaller time needed will chase up the car before
-		List<Double> rTimeL = new ArrayList<Double>(); //store time used for each fleet in ascending order
-		for (int i = 0; i < n; i++) {
-			Double time = map.get(dist[i]);
-			int m = rTimeL.size();
-			if (m == 0 || time > rTimeL.get(m - 1)) { 
-				rTimeL.add(time); //new fleet is created
-			}
-		}
-		return rTimeL.size();
-	}
+    public int carFleet(int target, int[] position, int[] speed) {
+        int n = position.length;
+
+        int[][] pairs = new int[n][2];
+        
+        for (int i = 0; i < n; i++) {
+            pairs[i][0] = position[i];
+            pairs[i][1] = speed[i];
+        }
+        
+        Arrays.sort(pairs, (a, b) -> a[0] - b[0]);
+        
+        double slowestTime = ((double) target - pairs[n - 1][0]) / pairs[n - 1][1];
+        int count = 1;
+        
+        for (int i = n - 2; i >= 0; i--) {
+            double currTime = ((double) target - pairs[i][0]) / pairs[i][1];
+
+            if (currTime > slowestTime) {
+                slowestTime = currTime;
+                count++;
+            }
+        }
+        
+        return count;
+    }
 }
