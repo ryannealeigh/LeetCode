cb1e378a259c8dc8a90d5e17063d8d65ae653934
Ryan Nealeigh <ryan.nealeigh@gmail.com>
2022-05-25 13:20:23 -0700

    Time: 164 ms (34.75%), Space: 85.4 MB (47.96%) - LeetHub

--- 853-car-fleet/853-car-fleet.java
+++ 853-car-fleet/853-car-fleet.java
@@ -12,11 +12,10 @@ class Solution {
         Arrays.sort(pairs, (a, b) -> a[0] - b[0]);
         
         double slowestTime = ((double) target - pairs[n - 1][0]) / pairs[n - 1][1];
-        double currTime;
         int count = 1;
         
         for (int i = n - 2; i >= 0; i--) {
-            currTime = ((double) target - pairs[i][0]) / pairs[i][1];
+            double currTime = ((double) target - pairs[i][0]) / pairs[i][1];
 
             if (currTime > slowestTime) {
                 slowestTime = currTime;
