e012090f27f5988de161598ea2c1845e6402367e
Ryan Nealeigh <ryan.nealeigh@gmail.com>
2022-05-25 07:12:53 -0700

    Time: 10 ms (94.36%), Space: 132.4 MB (33.04%) - LeetHub

--- 739-daily-temperatures/739-daily-temperatures.java
+++ 739-daily-temperatures/739-daily-temperatures.java
@@ -8,20 +8,15 @@ class Solution {
         for (int i = temperatures.length - 1; i >= 0; i--) {
             int currentTemp = temperatures[i];
             hottest = Math.max(hottest, currentTemp);
-            count = 1;
             if (hottest == currentTemp) {
-                result[i] = 0;
-            } else {
-                while (i + count < temperatures.length && temperatures[i + count] <= currentTemp) {
-                    if (result[i + count] == 0) {
-                        count = 0;
-                        break;
-                    }
-                    count += result[i + count];
-                }
-                result[i] = count;
+                continue;
             }
             
+            count = 1;
+            while (temperatures[i + count] <= currentTemp) {
+                count += result[i + count];
+            }
+            result[i] = count;
         }
 
         return result;
