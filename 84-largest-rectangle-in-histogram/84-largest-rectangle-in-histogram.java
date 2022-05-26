598c5e5c26093998da7562955d5345dd0f079b02
Ryan Nealeigh <ryan.nealeigh@gmail.com>
2022-05-26 12:54:16 -0700

    Time: 33 ms (84.18%), Space: 90 MB (46.08%) - LeetHub

--- 84-largest-rectangle-in-histogram/84-largest-rectangle-in-histogram.java
+++ 84-largest-rectangle-in-histogram/84-largest-rectangle-in-histogram.java
@@ -1,22 +1,21 @@
 public class Solution {
+
     public int largestRectangleArea(int[] heights) {
-        Deque<Integer> stack = new ArrayDeque<>();
-        stack.push(-1);
-        int length = heights.length;
+        Deque<int[]> stack = new ArrayDeque<>();
         int maxArea = 0;
-        for (int i = 0; i < length; i++) {
-            while ((stack.peek() != -1)
-                    && (heights[stack.peek()] >= heights[i])) {
-                int currentHeight = heights[stack.pop()];
-                int currentWidth = i - stack.peek() - 1;
-                maxArea = Math.max(maxArea, currentHeight * currentWidth);
+        
+        for (int i = 0; i < heights.length; i++) {
+            int start = i;
+            while (!stack.isEmpty() && stack.peek()[1] >= heights[i]) {
+                int[] prev = stack.pop();
+                maxArea = Math.max(maxArea, prev[1] * (i - prev[0]));
+                start = prev[0];
             }
-            stack.push(i);
+            stack.push(new int[]{start, heights[i]});
         }
-        while (stack.peek() != -1) {
-            int currentHeight = heights[stack.pop()];
-            int currentWidth = length - stack.peek() - 1;
-            maxArea = Math.max(maxArea, currentHeight * currentWidth);
+        while (!stack.isEmpty()) {
+            int[] prev = stack.pop();
+            maxArea = Math.max(maxArea, prev[1] * (heights.length - prev[0]));
         }
         return maxArea;
     }
