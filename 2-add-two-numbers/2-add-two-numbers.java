08f836e263eaf1f5d55a6cb5b1f4e09bae8cadff
Ryan Nealeigh <ryan.nealeigh@gmail.com>
2022-05-29 19:29:30 -0700

    Time: 3 ms (64.95%), Space: 48.2 MB (26.09%) - LeetHub

--- 2-add-two-numbers/2-add-two-numbers.java
+++ 2-add-two-numbers/2-add-two-numbers.java
@@ -9,55 +9,28 @@
  * }
  */
 class Solution {
-
     public ListNode addTwoNumbers(ListNode l1, ListNode l2) {
-        // create new result list node
-        // add numbers at current nodes
-        // store sum % 10 at current node in result list
-        // carry over remainder sum / 10 to next addition operation
-        ListNode result = new ListNode();
-        ListNode curr = result;
-        int remainder = 0, sum = 0;
-        ListNode prev = null;
-
-        while (l1 != null && l2 != null) {
-            sum = l1.val + l2.val + remainder;
-            curr.val = sum % 10;
-            curr.next = new ListNode();
-            prev = curr;
+        ListNode dummy = new ListNode(0);
+        ListNode curr = dummy;
+        int carry = 0;
+        int x, y;
+        
+        while (l1 != null || l2 != null) {
+            x = l1 == null ? 0 : l1.val;
+            y = l2 == null ? 0 : l2.val;
+            int val = x + y + carry;
+            curr.next = new ListNode(val % 10);
+            carry = val / 10;
             curr = curr.next;
-            remainder = sum / 10;
-            l1 = l1.next;
-            l2 = l2.next;
+            
+            if (l1 != null) l1 = l1.next;
+            if (l2 != null) l2 = l2.next;
         }
-
-        while (l1 != null) {
-            sum = l1.val + remainder;
-            curr.val = sum % 10;
-            curr.next = new ListNode();
-            prev = curr;
-            curr = curr.next;
-            remainder = sum / 10;
-            l1 = l1.next;
+        
+        if (carry != 0) {
+            curr.next = new ListNode(carry);
         }
-
-        while (l2 != null) {
-            sum = l2.val + remainder;
-            curr.val = sum % 10;
-            curr.next = new ListNode();
-            prev = curr;
-            curr = curr.next;
-            remainder = sum / 10;
-            l2 = l2.next;
-        }
-
-        //add final remainder if not 0
-        if (remainder == 0) {
-            prev.next = null;
-        } else {
-            curr.val = remainder;
-        }
-
-        return result;
+        
+        return dummy.next;
     }
 }
