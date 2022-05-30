93e90be02b7bf35dcfe16b497b3e1ea1b9da1a6f
Ryan Nealeigh <ryan.nealeigh@gmail.com>
2022-05-29 19:29:47 -0700

    Time: 3 ms (64.95%), Space: 48 MB (31.73%) - LeetHub

--- 2-add-two-numbers/2-add-two-numbers.java
+++ 2-add-two-numbers/2-add-two-numbers.java
@@ -9,12 +9,13 @@
  * }
  */
 class Solution {
+
     public ListNode addTwoNumbers(ListNode l1, ListNode l2) {
         ListNode dummy = new ListNode(0);
         ListNode curr = dummy;
         int carry = 0;
         int x, y;
-        
+
         while (l1 != null || l2 != null) {
             x = l1 == null ? 0 : l1.val;
             y = l2 == null ? 0 : l2.val;
@@ -22,15 +23,15 @@ class Solution {
             curr.next = new ListNode(val % 10);
             carry = val / 10;
             curr = curr.next;
-            
+
             if (l1 != null) l1 = l1.next;
             if (l2 != null) l2 = l2.next;
         }
-        
+
         if (carry != 0) {
             curr.next = new ListNode(carry);
         }
-        
+
         return dummy.next;
     }
 }
