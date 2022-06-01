e83101b6104f8d1a870df8165b54067427c2f149
Ryan Nealeigh <ryan.nealeigh@gmail.com>
2022-05-31 19:39:48 -0700

    Time: 0 ms (100.00%), Space: 41 MB (67.83%) - LeetHub

--- 100-same-tree/100-same-tree.java
+++ 100-same-tree/100-same-tree.java
@@ -14,13 +14,34 @@
  * }
  */
 class Solution {
-    public boolean isSameTree(TreeNode p, TreeNode q) {
+    private boolean check(TreeNode p, TreeNode q) {
         if (p == null && q == null) {
             return true;
         }
-        if (p == null ^ q == null) {
+        if (p == null || q == null || p.val != q.val) {
             return false;
         }
-        return p.val == q.val && isSameTree(p.left, q.left) && isSameTree(p.right, q.right);
+        return true;
+    }
+    
+    public boolean isSameTree(TreeNode p, TreeNode q) {
+        ArrayDeque<TreeNode> queue = new ArrayDeque<>();
+        if (p == null && q == null) return true;
+        if (!check(p, q)) return false;
+        queue.add(p);
+        queue.add(q);
+        
+        while (!queue.isEmpty()) {
+            p = queue.poll();
+            q = queue.poll();
+            if (!check(p.left, q.left)) return false;
+            if (p.left != null) queue.add(p.left);
+            if (q.left != null) queue.add(q.left);
+            if (!check(p.right, q.right)) return false;
+            if (p.right != null) queue.add(p.right);
+            if (q.right != null) queue.add(q.right);
+        }
+        
+        return true;
     }
 }
