9ff5899dbafd676e61d8e756284d0c2fea008d79
Ryan Nealeigh <ryan.nealeigh@gmail.com>
2022-06-01 10:14:52 -0700

    Time: 4 ms (54.90%), Space: 43.8 MB (67.21%) - LeetHub

---
+++ 105-construct-binary-tree-from-preorder-and-inorder-traversal/105-construct-binary-tree-from-preorder-and-inorder-traversal.java
@@ -0,0 +1,44 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+    int i = 0;
+    public TreeNode buildTree(int[] preorder, int[] inorder) {
+        HashMap<Integer, Integer> map = new HashMap<>();
+        for (int i = 0; i < inorder.length; i++) {
+            map.put(inorder[i], i);
+        }
+        
+        int n = preorder.length;
+        
+        return makeTree(preorder, map, 0, n - 1);
+    }
+    
+    private TreeNode makeTree(int[] preorder, HashMap<Integer, Integer> map, int left, int right) {
+        int val = preorder[i++];
+        TreeNode node = new TreeNode(val);
+        int pivot = map.get(val);
+        
+        if (pivot - left > 0) {
+            node.left = makeTree(preorder, map, left, pivot - 1);
+        }
+        
+        if (right - pivot > 0) {
+            node.right = makeTree(preorder, map, pivot + 1, right);
+        }
+        
+        return node;
+    }
+}
