f9a933b194baa39aadb758358c780dda1fa6ba0b
Ryan Nealeigh <ryan.nealeigh@gmail.com>
2022-06-04 15:02:42 -0700

    Time: 2 ms (66.99%), Space: 44.5 MB (55.27%) - LeetHub

--- 46-permutations/46-permutations.java
+++ 46-permutations/46-permutations.java
@@ -1,27 +1,25 @@
 class Solution {
-    public List<List<Integer>> permute(int[] nums) {
-        int n = nums.length;
-        
-        HashSet<Integer> used;
+    public List<List<Integer>> permute(int[] nums) {        
+        boolean[] used = new boolean[nums.length];
         // we want to start at each index for all possible permutations
         List<List<Integer>> result = new ArrayList<>();
-        backtrack(nums, new ArrayList<Integer>(), new HashSet<Integer>(), result);
+        backtrack(nums, new ArrayList<Integer>(), used, result);
         
         return result;
     }
     
-    private void backtrack(int[] nums, List<Integer> curr, HashSet<Integer> used, List<List<Integer>> result) {
+    private void backtrack(int[] nums, List<Integer> curr, boolean[] used, List<List<Integer>> result) {
         if (curr.size() == nums.length) {
             result.add(new ArrayList<>(curr));
             return;
         }
         
         for (int i = 0; i < nums.length; i++) {
-            if (!used.contains(i)) {
-                used.add(i);
+            if (!used[i]) {
+                used[i] = true;
                 curr.add(nums[i]);
                 backtrack(nums, curr, used, result);
-                used.remove(i);
+                used[i] = false;
                 curr.remove(curr.size() - 1);
             }
         }
