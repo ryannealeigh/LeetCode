6cd30f7bc4d0a7d9d17d1103f1c36cdd9f76e15f
Ryan Nealeigh <ryan.nealeigh@gmail.com>
2022-06-12 16:24:06 -0700

    Time: 28 ms (72.55%), Space: 42.9 MB (67.50%) - LeetHub

--- 133-clone-graph/133-clone-graph.java
+++ 133-clone-graph/133-clone-graph.java
@@ -19,20 +19,13 @@ class Solution {
             return node;
         }
 
-        // If the node was already visited before.
-        // Return the clone from the visited dictionary.
         if (visited.containsKey(node)) {
             return visited.get(node);
         }
-
-        // Create a clone for the given node.
-        // Note that we don't have cloned neighbors as of now, hence [].
         Node cloneNode = new Node(node.val, new ArrayList());
-        // The key is original node and value being the clone node.
+
         visited.put(node, cloneNode);
 
-        // Iterate through the neighbors to generate their clones
-        // and prepare a list of cloned neighbors to be added to the cloned node.
         for (Node neighbor: node.neighbors) {
             cloneNode.neighbors.add(cloneGraph(neighbor));
         }
