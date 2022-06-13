9e195051843caedc8645246c2d86288cfd524b92
Ryan Nealeigh <ryan.nealeigh@gmail.com>
2022-06-13 11:30:36 -0700

    Time: 4 ms (96.43%), Space: 98.1 MB (40.06%) - LeetHub

--- 234-palindrome-linked-list/234-palindrome-linked-list.java
+++ 234-palindrome-linked-list/234-palindrome-linked-list.java
@@ -9,12 +9,8 @@
  * }
  */
 class Solution {
+
     public boolean isPalindrome(ListNode head) {
-        // cut the list in half and compare the nodes
-        // find the halfway mark
-        // reverse the first half
-        // then compare the remainders
-        
         ListNode slow = head;
         ListNode fast = head;
         int i = 0;
@@ -23,12 +19,11 @@ class Solution {
             slow = slow.next;
             fast = fast.next.next;
         }
-                
+
         if (fast != null) {
             slow = slow.next;
         }
-        
-        // reverse first i nodes
+
         ListNode prev = null;
         ListNode curr = head;
         for (int j = 0; j < i; j++) {
@@ -37,10 +32,7 @@ class Solution {
             prev = curr;
             curr = temp;
         }
-        
-        // now prev has our head
-        // compare nodes in prev list to nodes at slow
-        
+
         for (int j = 0; j < i; j++) {
             if (prev.val != slow.val) {
                 return false;
@@ -48,7 +40,7 @@ class Solution {
             prev = prev.next;
             slow = slow.next;
         }
-        
+
         return true;
     }
 }
