17de9b001e1cb9390382d0fc22c9a376ff636a3f
Ryan Nealeigh <ryan.nealeigh@gmail.com>
2022-06-12 21:29:45 -0700

    Time: 1 ms (43.45%), Space: 40.9 MB (43.97%) - LeetHub

---
+++ 62-unique-paths/62-unique-paths.java
@@ -0,0 +1,21 @@
+class Solution {
+    public int uniquePaths(int m, int n) {
+        int[][] dp = new int[m][n];
+        
+        for (int i = 0; i < n; i++) {
+            dp[0][i] = 1;
+        }
+        
+        for (int i = 0; i < m; i++) {
+            dp[i][0] = 1;
+        }
+        
+        for (int row = 1; row < m; row++) {
+            for (int col = 1; col < n; col++) {
+                dp[row][col] = dp[row - 1][col] + dp[row][col - 1];
+            }
+        }
+        
+        return dp[m - 1][n - 1];
+    }
+}
