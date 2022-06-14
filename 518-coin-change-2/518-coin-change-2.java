d0d4e940c6d9dfa5b849bf7324f2d231df08b6fe
Ryan Nealeigh <ryan.nealeigh@gmail.com>
2022-06-14 09:15:13 -0700

    Time: 4 ms (79.55%), Space: 41.9 MB (66.83%) - LeetHub

--- 518-coin-change-2/518-coin-change-2.java
+++ 518-coin-change-2/518-coin-change-2.java
@@ -1,33 +1,13 @@
 class Solution {
-    public int change(int amount, int[] coins) {
-        if (amount == 0) {
-            return 1;
-        }
-        int n = coins.length;
-        // coins are rows and amount is column
-        int[][] dp = new int[n + 1][amount + 1];
-        
-        for (int i = 1; i <= n; i++) {
-            int coin = coins[i - 1];            // get coin value corresponding to this row
+  public int change(int amount, int[] coins) {
+    int[] dp = new int[amount + 1];
+    dp[0] = 1;
 
-            for (int j = 1; j <= amount; j++) {     // j is equal to current amount we are considering
-                int remainder = j - coin;
-                
-                if (remainder == 0) {
-                    dp[i][j] = 1;
-                } else if (remainder > 0) {
-                    for (int row = 1; row <= i; row++) {
-                        dp[i][j] += dp[row][remainder];
-                    }
-                }
-            }
-        }
-        
-        int total = 0;
-        // add up last column corresponding to amount
-        for (int i = 0; i <= n; i++) {
-            total += dp[i][amount];
-        }
-        return total;
+    for (int coin : coins) {
+      for (int x = coin; x < amount + 1; ++x) {
+        dp[x] += dp[x - coin];
+      }
     }
+    return dp[amount];
+  }
 }
