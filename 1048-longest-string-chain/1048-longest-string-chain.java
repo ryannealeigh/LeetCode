8b8abd14f6f56e8586b4e6c5f867675eab707edb
Ryan Nealeigh <ryan.nealeigh@gmail.com>
2022-06-15 09:49:03 -0700

    Time: 74 ms (37.27%), Space: 53.9 MB (55.98%) - LeetHub

--- 1048-longest-string-chain/1048-longest-string-chain.java
+++ 1048-longest-string-chain/1048-longest-string-chain.java
@@ -1,43 +1,38 @@
 class Solution {
-    public int longestStrChain(String[] words) {
 
+    public int longestStrChain(String[] words) {
         HashSet<String> wordSet = new HashSet<>();
         for (String word : words) {
             wordSet.add(word);
         }
-        
+
         HashMap<String, Integer> counts = new HashMap<>();
-        
+
         // dynamic programming
         Arrays.sort(words, (a, b) -> Integer.compare(a.length(), b.length()));
-        
+
         for (int i = words.length - 1; i >= 0; i--) {
             String parent = words[i];
             StringBuilder child = new StringBuilder();
             child.append(parent);
             for (int j = 0; j < child.length(); j++) {
-                
                 char c = child.charAt(j);
                 child.deleteCharAt(j);
                 if (wordSet.contains(child.toString())) {
-
-                    // get the existing childLength or default of 1
-                    // we don't want to overwrite this child if it is already greater
-                    // otherwise, we overwrite child maxLen with parentLength + 1
                     int childLen = counts.getOrDefault(child.toString(), 1);
                     int parentLen = counts.getOrDefault(parent, 1) + 1;
-                    
+
                     counts.put(child.toString(), Math.max(childLen, parentLen));
                 }
                 child.insert(j, c);
             }
         }
-        
+
         int result = 1;
         for (String key : counts.keySet()) {
             result = Math.max(result, counts.get(key));
         }
-        
+
         return result;
     }
 }
