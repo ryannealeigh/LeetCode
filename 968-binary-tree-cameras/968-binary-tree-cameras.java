6acaf154832d5b7bbff4be8e34388b38aef04228
Ryan Nealeigh <ryan.nealeigh@gmail.com>
2022-06-17 11:14:24 -0700

    Time: 3 ms (12.16%), Space: 44.7 MB (6.20%) - LeetHub

--- 968-binary-tree-cameras/968-binary-tree-cameras.java
+++ 968-binary-tree-cameras/968-binary-tree-cameras.java
@@ -1,24 +1,29 @@
 class Solution {
+    int ans;
+    Set<TreeNode> covered;
     public int minCameraCover(TreeNode root) {
-        int[] ans = solve(root);
-        return Math.min(ans[1], ans[2]);
+        ans = 0;
+        covered = new HashSet();
+        covered.add(null);
+
+        dfs(root, null);
+        return ans;
     }
 
-    // 0: Strict ST; All nodes below this are covered, but not this one
-    // 1: Normal ST; All nodes below and incl this are covered - no camera
-    // 2: Placed camera; All nodes below this are covered, plus camera here
-    public int[] solve(TreeNode node) {
-        if (node == null)
-            return new int[]{0, 0, 99999};
+    public void dfs(TreeNode node, TreeNode par) {
+        if (node != null) {
+            dfs(node.left, node);
+            dfs(node.right, node);
 
-        int[] L = solve(node.left);
-        int[] R = solve(node.right);
-        int mL12 = Math.min(L[1], L[2]);
-        int mR12 = Math.min(R[1], R[2]);
-
-        int d0 = L[1] + R[1];
-        int d1 = Math.min(L[2] + mR12, R[2] + mL12);
-        int d2 = 1 + Math.min(L[0], mL12) + Math.min(R[0], mR12);
-        return new int[]{d0, d1, d2};
+            if (par == null && !covered.contains(node) ||
+                    !covered.contains(node.left) ||
+                    !covered.contains(node.right)) {
+                ans++;
+                covered.add(node);
+                covered.add(par);
+                covered.add(node.left);
+                covered.add(node.right);
+            }
+        }
     }
 }
