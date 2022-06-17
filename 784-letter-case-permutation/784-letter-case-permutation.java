fb8a551bde55f9591c40dc04a8f38a0179e0190c
Ryan Nealeigh <ryan.nealeigh@gmail.com>
2022-06-17 16:27:26 -0700

    Time: 2 ms (96.10%), Space: 42.9 MB (94.20%) - LeetHub

--- 784-letter-case-permutation/784-letter-case-permutation.java
+++ 784-letter-case-permutation/784-letter-case-permutation.java
@@ -12,17 +12,12 @@ class Solution {
             result.add(curr.toString());
             return;
         }
-        
-        if (index == s.length()) {
-            return;
-        }
-                
+
         char c = s.charAt(index);
         if (Character.isLetter(c)) {
             curr.append(Character.toLowerCase(c));
             backtrack(s, curr, index + 1, result);
             curr.deleteCharAt(curr.length() - 1);
-            
             curr.append(Character.toUpperCase(c));
             backtrack(s, curr, index + 1, result);
             curr.deleteCharAt(curr.length() - 1);
