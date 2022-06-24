ba7004d7e77734378c70c5b00b96460b7446a343
Ryan Nealeigh <ryan.nealeigh@gmail.com>
2022-06-24 14:00:33 -0700

    Time: 1 ms (86.52%), Space: 44.2 MB (36.73%) - LeetHub

--- 684-redundant-connection/684-redundant-connection.java
+++ 684-redundant-connection/684-redundant-connection.java
@@ -1,26 +1,36 @@
 class Solution {
     class UnionFind {
         int[] root;
+        int[] rank;
+        
         public UnionFind(int size) {
             root = new int[size];
+            rank = new int[size];
             for (int i = 0; i < size; i++) {
                 root[i] = i;
+                rank[i] = 1;
             }
         }
         
         public int find(int x) {
-            return root[x];
+            while (x != root[x]) {
+                x = root[x];
+            }
+            return x;
         }
         
         public void union(int x, int y) {
-            int rootX = root[x];
-            int rootY = root[y];
+            int rootX = find(x);
+            int rootY = find(y);
             
             if (rootX != rootY) {
-                for (int i = 0; i < root.length; i++) {
-                    if (root[i] == rootY) {
-                        root[i] = rootX;
-                    }
+                if (rank[rootX] > rank[rootY]) {
+                    root[rootY] = rootX;
+                } else if (rank[rootX] < rank[rootY]) {
+                    root[rootX] = rootY;
+                } else {
+                    root[rootY] = rootX;
+                    rank[rootX] += 1;
                 }
             }
         }
