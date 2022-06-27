2eca65e0e7e8be064e4c7fc6415bc2336b996903
Ryan Nealeigh <ryan.nealeigh@gmail.com>
2022-06-27 14:17:30 -0700

    Time: 12 ms (41.77%), Space: 54.6 MB (27.54%) - LeetHub

--- 1689-partitioning-into-minimum-number-of-deci-binary-numbers/1689-partitioning-into-minimum-number-of-deci-binary-numbers.java
+++ 1689-partitioning-into-minimum-number-of-deci-binary-numbers/1689-partitioning-into-minimum-number-of-deci-binary-numbers.java
@@ -3,11 +3,9 @@ class Solution {
         // determined by the largetst digit in the string
         
         int max = 0;
-        char[] chars = n.toCharArray();
         
-        for (int i = 0; i < chars.length; i++) {
-            int val = Character.getNumericValue(chars[i]);
-            max = Math.max(max, val);
+        for (int i = 0; i < n.length(); i++) {
+            max = Math.max(max, n.charAt(i) - '0');
         }
         
         return max;
