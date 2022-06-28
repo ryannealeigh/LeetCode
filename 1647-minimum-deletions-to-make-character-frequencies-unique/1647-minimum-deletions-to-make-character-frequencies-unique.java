6641e618e932aac045d2f32324571257576ac69b
Ryan Nealeigh <ryan.nealeigh@gmail.com>
2022-06-27 22:06:52 -0700

    Time: 142 ms (5.77%), Space: 68 MB (19.61%) - LeetHub

--- 1647-minimum-deletions-to-make-character-frequencies-unique/1647-minimum-deletions-to-make-character-frequencies-unique.java
+++ 1647-minimum-deletions-to-make-character-frequencies-unique/1647-minimum-deletions-to-make-character-frequencies-unique.java
@@ -1,17 +1,15 @@
 class Solution {
+
     public int minDeletions(String s) {
-        // count up all characters
-        // then add them to a set one by one making sure that they are unique and deleting
-        // some characters as necessary
         HashMap<Character, Integer> counts = new HashMap<>();
-        
+
         for (int i = 0; i < s.length(); i++) {
             char c = s.charAt(i);
             counts.put(c, counts.getOrDefault(c, 0) + 1);
         }
-        
+
         HashSet<Integer> unique = new HashSet<>();
-        
+
         int removed = 0;
         for (Character c : counts.keySet()) {
             int val = counts.get(c);
@@ -25,7 +23,7 @@ class Solution {
             }
             unique.add(val);
         }
-        
+
         return removed;
     }
 }
