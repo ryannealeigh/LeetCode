95a052a93a13d036f53cd1e22d947d0746633e1e
Ryan Nealeigh <ryan.nealeigh@gmail.com>
2022-06-30 12:38:51 -0700

    Time: 124 ms (27.81%), Space: 43.1 MB (84.47%) - LeetHub

--- 55-jump-game/55-jump-game.java
+++ 55-jump-game/55-jump-game.java
@@ -1,38 +1,25 @@
-class Solution {
+enum Index {
+    GOOD, BAD, UNKNOWN
+}
+
+public class Solution {
     public boolean canJump(int[] nums) {
-        // dfs with memoization
-        // for int j = 0 to nums[i]
-        
-        Boolean[] dp = new Boolean[nums.length];
-        dp[nums.length - 1] = true;
-        return dfs(0, nums, dp);
-    }
-    
-    private boolean dfs(int index, int[] nums, Boolean[] dp) {
-        if (index == nums.length - 1) {
-            return true;
+        Index[] memo = new Index[nums.length];
+        for (int i = 0; i < memo.length; i++) {
+            memo[i] = Index.UNKNOWN;
         }
-        
-        if (index >= nums.length) {
-            return false;
-        }
-        
-        if (dp[index] != null) {
-            return dp[index];
-        }
-        
-        for (int i = 1; i <= nums[index]; i++) {
-            int nextPosition = index + i;
-            if (nextPosition > nums.length - 1) {
-                break;
-            }
-            if (dfs(nextPosition, nums, dp)) {
-                dp[nextPosition] = true;
-                return dp[nextPosition];
+        memo[memo.length - 1] = Index.GOOD;
+
+        for (int i = nums.length - 2; i >= 0; i--) {
+            int furthestJump = Math.min(i + nums[i], nums.length - 1);
+            for (int j = i + 1; j <= furthestJump; j++) {
+                if (memo[j] == Index.GOOD) {
+                    memo[i] = Index.GOOD;
+                    break;
+                }
             }
         }
-        
-        dp[index] = false;
-        return false;
+
+        return memo[0] == Index.GOOD;
     }
 }
