e579f0dd63219d74a958728de5e0c9fdce9d724b
Ryan Nealeigh <ryan.nealeigh@gmail.com>
2022-07-01 17:16:40 -0700

    Time: 1 ms (98.77%), Space: 42.6 MB (30.02%) - LeetHub

---
+++ 2224-minimum-number-of-operations-to-convert-time/2224-minimum-number-of-operations-to-convert-time.java
@@ -0,0 +1,44 @@
+class Solution {
+    public int convertTime(String current, String correct) {
+        int result = 0;
+        
+        int currentHour = Integer.parseInt(current.substring(0, 2));
+        int correctHour = Integer.parseInt(correct.substring(0, 2));
+        
+        int hourDiff = correctHour - currentHour;
+                
+        int currentMinutes = Integer.parseInt(current.substring(3, 5));
+        int correctMinutes = Integer.parseInt(correct.substring(3, 5));
+        
+        int minuteDiff = correctMinutes - currentMinutes;
+        
+        if (currentMinutes > correctMinutes) {
+            minuteDiff = (60 - currentMinutes) + correctMinutes;
+        }
+        
+        if (currentMinutes + minuteDiff >= 60) {
+            hourDiff--;
+        }
+        
+        int remainder = 0;
+        if (minuteDiff / 60 == 1) {
+            result++;
+            remainder = minuteDiff % 60;
+            minuteDiff %= 60;
+        }
+        
+        result += minuteDiff / 60;
+        minuteDiff %= 60;
+        
+        result += minuteDiff / 15;
+        minuteDiff %= 15;
+        
+        result += minuteDiff / 5;
+        minuteDiff %= 5;
+        
+        result += minuteDiff;
+        result += hourDiff;
+        
+        return result;
+    }
+}
