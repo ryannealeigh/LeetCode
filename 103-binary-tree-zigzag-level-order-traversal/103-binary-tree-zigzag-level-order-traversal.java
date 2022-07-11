c4e7cf355229440d1fbfc78d45ddf79a2a87aa58
Ryan Nealeigh <ryan.nealeigh@gmail.com>
2022-07-11 15:09:50 -0700

    Time: 1 ms (96.21%), Space: 40.9 MB (94.64%) - LeetHub

---
+++ 103-binary-tree-zigzag-level-order-traversal/103-binary-tree-zigzag-level-order-traversal.java
@@ -0,0 +1,53 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+    public List<List<Integer>> zigzagLevelOrder(TreeNode root) {
+        // bfs for level order
+        // add root to queue
+        // traverse queue in order adding children to queue
+        // traverse queue in reverse order adding children in reverse order
+        if (root == null) {
+            return List.of();
+        }
+        ArrayDeque<TreeNode> queue = new ArrayDeque<>();
+        queue.add(root);
+        List<List<Integer>> result = new ArrayList<>();
+        
+        boolean forward = true;
+        while (!queue.isEmpty()) {
+            int n = queue.size();
+            List<Integer> row = new ArrayList<>();
+            for (int i = 0; i < n; i++) {
+                if (forward) {
+                    TreeNode node = queue.poll();
+                    row.add(node.val);
+                    if (node.left != null) queue.add(node.left);
+                    if (node.right != null) queue.add(node.right);
+                } else {
+                    TreeNode node = queue.pollLast();
+                    row.add(node.val);
+                    if (node.right != null) queue.addFirst(node.right);
+                    if (node.left != null) queue.addFirst(node.left);
+                }
+            }
+            forward = !forward;
+            result.add(row);
+
+        }
+        
+        return result;
+    }
+}
