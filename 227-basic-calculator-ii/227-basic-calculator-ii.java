d7c6841463a91e1de3f1fc0071f40ab0b8f0bb77
Ryan Nealeigh <ryan.nealeigh@gmail.com>
2022-07-25 15:32:29 -0700

    Time: 8 ms (95.80%), Space: 43.1 MB (86.06%) - LeetHub

--- 227-basic-calculator-ii/227-basic-calculator-ii.java
+++ 227-basic-calculator-ii/227-basic-calculator-ii.java
@@ -1,52 +1,28 @@
 class Solution {
-
     public int calculate(String s) {
-        // one sweep for multiplication and division
-        // one sweep for addition and substraction
-        // split string on operators
-
-        String str = s.replace(" ", "");
-        String[] split = str.split("(?=[-+*/])|(?<=[-+*/])");
-
-        ArrayDeque<String> stack = new ArrayDeque<>();
-
-        for (String val : split) {
-            // if stack contains a value and current operator is multiply or divide, handle it now
-            if (!stack.isEmpty() && (stack.peekLast().equals("*") || stack.peekLast().equals("/"))) {
-                String op = stack.pollLast();
-                long prevLong = Long.parseLong(stack.pollLast());
-                long currLong = Long.parseLong(val);
-                
-                long intermediate = prevLong;
-
-                if (op.equals("*")) {
-                    intermediate *= currLong;
-                    stack.add(Long.toString(intermediate));
-                } else {
-                    intermediate /= currLong;
-                    stack.add(Long.toString(intermediate));
+        if (s == null || s.isEmpty()) return 0;
+        int length = s.length();
+        int currentNumber = 0, lastNumber = 0, result = 0;
+        char operation = '+';
+        for (int i = 0; i < length; i++) {
+            char currentChar = s.charAt(i);
+            if (Character.isDigit(currentChar)) {
+                currentNumber = (currentNumber * 10) + (currentChar - '0');
+            }
+            if (!Character.isDigit(currentChar) && !Character.isWhitespace(currentChar) || i == length - 1) {
+                if (operation == '+' || operation == '-') {
+                    result += lastNumber;
+                    lastNumber = (operation == '+') ? currentNumber : -currentNumber;
+                } else if (operation == '*') {
+                    lastNumber = lastNumber * currentNumber;
+                } else if (operation == '/') {
+                    lastNumber = lastNumber / currentNumber;
                 }
-            } else { // if stack is empty or current operator is not * or /
-                stack.add(val);
+                operation = currentChar;
+                currentNumber = 0;
             }
         }
-
-        while (stack.size() > 1) {
-            long prevLong = Long.parseLong(stack.pollFirst());
-            String op = stack.pollFirst();
-            long currLong = Long.parseLong(stack.pollFirst());
-
-            long intermediate = prevLong;
-
-            if (op.equals("-")) {
-                intermediate -= currLong;
-                stack.addFirst(Long.toString(intermediate));
-            } else {
-                intermediate += currLong;
-                stack.addFirst(Long.toString(intermediate));
-            }
-        }
-
-        return Integer.parseInt(stack.peek());
+        result += lastNumber;
+        return result;
     }
 }
