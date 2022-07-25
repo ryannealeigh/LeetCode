df4384c83cc1755de312b914e2a1909b3b572410
Ryan Nealeigh <ryan.nealeigh@gmail.com>
2022-07-24 18:05:51 -0700

    Time: 65 ms (54.31%), Space: 48.5 MB (44.43%) - LeetHub

---
+++ 16-3sum-closest/16-3sum-closest.java
@@ -0,0 +1,34 @@
+class Solution {
+    public int threeSumClosest(int[] nums, int target) {
+        // add up all 3 tuples in nums and compare diff to target
+        // sort first then do two pointer for O(n^2) TC
+        
+        Arrays.sort(nums);
+        int best = 0;
+        int minDiff = Integer.MAX_VALUE;
+        
+        for (int i = 0; i < nums.length - 2; i++) {
+            int left = i + 1, right = nums.length - 1;
+
+            int currTarget = target - nums[i];
+            while (left < right) {
+                int sum = nums[left] + nums[right];
+                int diff = Math.abs(currTarget - sum);
+                if (diff < minDiff) {
+                    minDiff = diff;
+                    best = sum + nums[i];
+                }
+                
+                if (sum == currTarget) {
+                    return target;
+                } else if (sum < currTarget) {
+                    left++;
+                } else {
+                    right--;
+                }
+            }
+        }
+        
+        return best;
+    }
+}
