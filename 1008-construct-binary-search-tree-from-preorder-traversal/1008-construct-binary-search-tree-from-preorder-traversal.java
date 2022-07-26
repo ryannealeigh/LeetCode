afc77087705f2b08161f174284aa40d689803fc5
Ryan Nealeigh <ryan.nealeigh@gmail.com>
2022-07-26 14:12:34 -0700

    Time: 1 ms (66.05%), Space: 42.4 MB (28.31%) - LeetHub

--- 1008-construct-binary-search-tree-from-preorder-traversal/1008-construct-binary-search-tree-from-preorder-traversal.java
+++ 1008-construct-binary-search-tree-from-preorder-traversal/1008-construct-binary-search-tree-from-preorder-traversal.java
@@ -12,17 +12,12 @@ class Solution {
     }
 
     public TreeNode helper(int lower, int upper) {
-        // if all elements from preorder are used
-        // then the tree is constructed
         if (idx == n) return null;
 
         int val = preorder[idx];
-        // if the current element
-        // couldn't be placed here to meet BST requirements
+
         if (val < lower || val > upper) return null;
 
-        // place the current element
-        // and recursively construct subtrees
         idx++;
         TreeNode root = new TreeNode(val);
         root.left = helper(lower, val);
