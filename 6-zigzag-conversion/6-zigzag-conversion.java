375cf4bff6000b9b0884fe13003cb11be9404b87
Ryan Nealeigh <ryan.nealeigh@gmail.com>
2022-07-27 14:40:46 -0700

    Time: 19 ms (45.39%), Space: 52 MB (36.71%) - LeetHub

--- 6-zigzag-conversion/6-zigzag-conversion.java
+++ 6-zigzag-conversion/6-zigzag-conversion.java
@@ -1,40 +1,42 @@
 class Solution {
+
     public String convert(String s, int numRows) {
         if (numRows == 1) {
             return s;
         }
-        
+
         HashMap<Integer, StringBuilder> map = new HashMap<>();
-        
+
         for (int i = 0; i < numRows; i++) {
             map.put(i, new StringBuilder());
         }
-        
+
         int row = 0;
         boolean down = true;
-        
+
         for (int i = 0; i < s.length(); i++) {
             map.get(row).append(s.charAt(i));
 
-            if (down && row < numRows - 1) {
+            if (down && row == numRows - 1) {
+                down = false;
+            }
+            if (!down && row == 0) {
+                down = true;
+            }
+
+            if (down) {
                 row++;
-                if (row == numRows - 1) {
-                    down = false;
-                }
-            } else if (!down && row > 0) {
+            } else {
                 row--;
-                if (row == 0) {
-                    down = true;
-                }
             }
         }
-        
+
         StringBuilder result = map.get(0);
-        
+
         for (int i = 1; i < numRows; i++) {
             result.append(map.get(i));
         }
-        
+
         return result.toString();
     }
 }
