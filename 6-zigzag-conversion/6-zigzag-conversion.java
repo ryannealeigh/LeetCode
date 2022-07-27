9676f1d1dc956f148a902a49376d99d9b22f35e9
Ryan Nealeigh <ryan.nealeigh@gmail.com>
2022-07-27 14:39:58 -0700

    Time: 21 ms (41.90%), Space: 51.5 MB (38.48%) - LeetHub

--- 6-zigzag-conversion/6-zigzag-conversion.java
+++ 6-zigzag-conversion/6-zigzag-conversion.java
@@ -1,38 +1,40 @@
 class Solution {
     public String convert(String s, int numRows) {
-
-        if (numRows == 1 || numRows >= s.length()) {
+        if (numRows == 1) {
             return s;
         }
         
-        List<StringBuilder> rows = new ArrayList<>();
+        HashMap<Integer, StringBuilder> map = new HashMap<>();
+        
         for (int i = 0; i < numRows; i++) {
-            rows.add(new StringBuilder());
+            map.put(i, new StringBuilder());
         }
         
         int row = 0;
         boolean down = true;
         
         for (int i = 0; i < s.length(); i++) {
-            rows.get(row).append(s.charAt(i));
-            if (down && row == numRows - 1) {
-                down = false;
-            }
-            if (!down && row == 0) {
-                down = true;
-            }
-            
-            if (down) {
+            map.get(row).append(s.charAt(i));
+
+            if (down && row < numRows - 1) {
                 row++;
-            } else {
+                if (row == numRows - 1) {
+                    down = false;
+                }
+            } else if (!down && row > 0) {
                 row--;
+                if (row == 0) {
+                    down = true;
+                }
             }
         }
         
-        for (int i = 1; i < rows.size(); i++) {
-            rows.get(0).append(rows.get(i));
+        StringBuilder result = map.get(0);
+        
+        for (int i = 1; i < numRows; i++) {
+            result.append(map.get(i));
         }
         
-        return rows.get(0).toString();
+        return result.toString();
     }
 }
