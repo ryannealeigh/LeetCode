35addf948b096175ec1ab37aabf572f04bca3d12
Ryan Nealeigh <ryan.nealeigh@gmail.com>
2022-08-03 16:45:06 -0700

    Time: 0 ms (100.00%), Space: 38.9 MB (96.34%) - LeetHub

--- 62-unique-paths/62-unique-paths.java
+++ 62-unique-paths/62-unique-paths.java
@@ -1,21 +1,21 @@
 class Solution {
     public int uniquePaths(int m, int n) {
-        int[][] dp = new int[m][n];
+        // dp array of size m + 1 x n + 1
+        // recurrence relation -> dp[i][j] = dp[i][j - 1] + dp[i - 1][j]
+        // init dp[0][0] = 1
         
-        for (int i = 0; i < n; i++) {
-            dp[0][i] = 1;
-        }
+        int[][] dp = new int[m + 1][n + 1];
+        dp[1][1] = 1;
         
-        for (int i = 0; i < m; i++) {
-            dp[i][0] = 1;
-        }
-        
-        for (int row = 1; row < m; row++) {
-            for (int col = 1; col < n; col++) {
-                dp[row][col] = dp[row - 1][col] + dp[row][col - 1];
+        for (int i = 1; i < m + 1; i++) {
+            for (int j = 1; j < n + 1; j++) {
+                if (i == 1 && j == 1) {
+                    continue;
+                }
+                dp[i][j] = dp[i][j - 1] + dp[i - 1][j];
             }
         }
         
-        return dp[m - 1][n - 1];
+        return dp[m][n];
     }
 }
