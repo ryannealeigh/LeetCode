c55a7ea0ce663754009a635b1ac7d4102dffc3fb
Ryan Nealeigh <ryan.nealeigh@gmail.com>
2022-08-03 16:28:13 -0700

    Time: 21 ms (54.59%), Space: 58 MB (10.31%) - LeetHub

--- 378-kth-smallest-element-in-a-sorted-matrix/378-kth-smallest-element-in-a-sorted-matrix.java
+++ 378-kth-smallest-element-in-a-sorted-matrix/378-kth-smallest-element-in-a-sorted-matrix.java
@@ -1,16 +1,43 @@
 class Solution {
-    public int kthSmallest(int[][] matrix, int k) {
-        PriorityQueue<Integer> maxHeap = new PriorityQueue<>(Comparator.reverseOrder());
+    class Value {
+        int row;
+        int col;
+        int val;
         
-        for (int i = 0; i < matrix.length; i++) {
-            for (int j = 0; j < matrix[0].length; j++) {
-                maxHeap.add(matrix[i][j]);
-                if (maxHeap.size() > k) {
-                    maxHeap.poll();
-                }
-            }
+        public Value(int row, int col, int val) {
+            this.row = row;
+            this.col = col;
+            this.val = val;
+        }
+    }
+    
+    public int kthSmallest(int[][] matrix, int k) {
+        // like merge k sorted lists
+        // for this we can use a min heap
+        // add object with row, col, value for each row to heap
+        // then poll items out of the heap until we hit k items polled out
+        // when we poll an item out we also add the next item for it's row
+        int n = matrix.length;
+        
+        PriorityQueue<Value> heap = new PriorityQueue<>((a, b) -> Integer.compare(a.val, b.val));
+        
+        for (int i = 0; i < n; i++) {
+            heap.add(new Value(i, 0, matrix[i][0]));
         }
         
-        return maxHeap.peek();
+        int count = 0;
+        int prev = 0;
+        while (count < k) {
+            count++;
+            Value curr = heap.poll();
+            prev = curr.val;
+            if (curr.col < n - 1) {
+                int row = curr.row;
+                int newCol = curr.col + 1;
+                heap.add(new Value(curr.row, newCol, matrix[row][newCol]));
+            }
+        }
+                
+        return prev;
     }
 }
