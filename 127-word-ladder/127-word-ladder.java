6afe9a4ebfda0b64c1134e60cbd8450274796770
Ryan Nealeigh <ryan.nealeigh@gmail.com>
2022-08-04 16:28:34 -0700

    Time: 1083 ms (13.87%), Space: 42.5 MB (99.44%) - LeetHub

--- 127-word-ladder/127-word-ladder.java
+++ 127-word-ladder/127-word-ladder.java
@@ -1,57 +1,49 @@
 class Solution {
 
     public int ladderLength(String beginWord, String endWord, List<String> wordList) {
-        // trying bfs approach
-
-        HashMap<String, List<String>> patterns = new HashMap<>();
-        
-        wordList.add(beginWord);
-        for (String word : wordList) {
-            char[] chars = word.toCharArray();
-            for (int i = 0; i < chars.length; i++) {
-                char c = chars[i];
-                chars[i] = '*';
-                String pattern = new String(chars);
-                patterns.putIfAbsent(pattern, new ArrayList<String>());
-                patterns.get(pattern).add(word);
-                chars[i] = c;
-            }
+        if (wordList.contains(beginWord)) {
+            wordList.remove(beginWord);
         }
         
         ArrayDeque<String> queue = new ArrayDeque<>();
-        HashSet<String> used = new HashSet<>();
-
         queue.add(beginWord);
-        used.add(beginWord);
         
         int count = 0;
-        
         while (!queue.isEmpty()) {
             count++;
             
             int n = queue.size();
+
             for (int i = 0; i < n; i++) {
                 String curr = queue.poll();
                 if (curr.equals(endWord)) {
                     return count;
                 }
-                
-                char[] chars = curr.toCharArray();
-                for (int j = 0; j < curr.length(); j++) {
-                    char c = chars[j];
-                    chars[j] = '*';
-                    String pattern = new String(chars);
-                    for (String word : patterns.get(pattern)) {
-                        if (!used.contains(word)) {
-                            queue.add(word);
-                            used.add(word);
-                        }
+
+                List<String> toRemove = new ArrayList<>();
+                for (int j = 0; j < wordList.size(); j++) {
+                    String nextWord = wordList.get(j);
+                    if (offByOne(curr, nextWord)) {
+                        queue.add(nextWord);
+                        toRemove.add(nextWord);
                     }
-                    chars[j] = c;
                 }
+                
+                wordList.removeAll(toRemove);
             }
         }
 
         return 0;
+    }
+
+    private boolean offByOne(String a, String b) {
+        int offBy = 0;
+        for (int i = 0; i < a.length(); i++) {
+            if (a.charAt(i) != b.charAt(i)) {
+                offBy++;
+            }
+        }
+
+        return offBy == 1;
     }
 }
